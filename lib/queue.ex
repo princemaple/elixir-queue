4d2c3b9ab87269384ca8ef9c5109f7fcefee8ace
Po Chen <chenpaul914@gmail.com>
2016-12-26 23:59:52 +1100

    sync more APIs

--- lib/queue.ex
+++ lib/queue.ex
@@ -28,6 +28,11 @@ defmodule Qex do
     %__MODULE__{data: :queue.in(item, q)}
   end
 
+  @spec push_front(t, term) :: t
+  def push_front(%__MODULE__{data: q}, item) do
+    %__MODULE__{data: :queue.in_r(item, q)}
+  end
+
   @spec pop(t) :: {{:value, term}, t} | {:empty, t}
   def pop(%__MODULE__{data: q}) do
     case :queue.out(q) do
@@ -41,6 +46,34 @@ defmodule Qex do
     case :queue.out(q) do
       {{:value, v}, q} -> {v, %__MODULE__{data: q}}
       {:empty, _q} -> raise "Queue is empty"
+    end
+  end
+
+  @spec pop_back(t) :: {{:value, term}, t} | {:empty, t}
+  def pop_back(%__MODULE__{data: q}) do
+    case :queue.out_r(q) do
+      {{:value, v}, q} -> {{:value, v}, %__MODULE__{data: q}}
+      {:empty, q} -> {:empty, %__MODULE__{data: q}}
+    end
+  end
+
+  @spec pop_back!(t) :: {term, t} | no_return
+  def pop_back!(%__MODULE__{data: q}) do
+    case :queue.out_r(q) do
+      {{:value, v}, q} -> {v, %__MODULE__{data: q}}
+      {:empty, _q} -> raise "Queue is empty"
+    end
+  end
+
+  @spec reverse(t) :: t
+  def reverse(%__MODULE__{data: q}) do
+    %__MODULE__{data: :queue.reverse(q)}
+  end
+
+  @spec split(t, pos_integer) :: {t, t}
+  def split(%__MODULE__{data: q}, n) do
+    with {q1, q2} <- :queue.split(n, q) do
+      {%__MODULE__{data: q1}, %__MODULE__{data: q2}}
     end
   end
 end
