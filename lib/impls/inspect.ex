18efc2c3203a72712e9c3f58bda6acf3449257b0
Po Chen <chenpaul914@gmail.com>
2016-12-26 22:05:01 +1100

    dogfooding enum in inspect

--- lib/impls/inspect.ex
+++ lib/impls/inspect.ex
@@ -1,7 +1,7 @@
 defimpl Inspect, for: Queue do
   import Inspect.Algebra
 
-  def inspect(%Queue{in_list: in_list, out_list: out_list}, opts) do
-    concat ["<#Queue", to_doc(out_list ++ Enum.reverse(in_list), opts), ">"]
+  def inspect(%Queue{} = q, opts) do
+    concat ["<#Queue", to_doc(Enum.to_list(q), opts), ">"]
   end
 end
