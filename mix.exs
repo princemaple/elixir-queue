508bb8f8ac092020dad9f9ac07daae3965e379f8
Po Chen <chenpaul914@gmail.com>
2016-12-27 01:14:25 +1100

    cleanup and bump

--- mix.exs
+++ mix.exs
@@ -3,33 +3,20 @@ defmodule Qex.Mixfile do
 
   def project do
     [app: :qex,
-     version: "0.3.0",
+     version: "0.3.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
 
-     # hex
      description: description(),
      package: package()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
