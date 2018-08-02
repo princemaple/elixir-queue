c771198d54b666308cc7d9ac543af9d299edde9a
Po Chen <chenpaul914@gmail.com>
2018-08-02 11:09:46 +1000

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Qex.Mixfile do
   def project do
     [app: :qex,
      version: "0.3.4",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
